5135b698de13ac433c473a4baf578b893826ae3a
Shamim Ahmed Nahid <shamimofficial771@gmail.com>
2025-10-06 21:33:47 +0600

    Add files via upload

---
+++ turtle_star_flower_diagram.py
@@ -0,0 +1,18 @@
+import turtle as t
+
+screen = t.Screen()
+screen.setup(width = 1.0, height = 1.0)
+
+t.color("red","yellow")
+t.begin_fill()
+t.speed(0)
+
+while True:
+    t.forward(400)
+    t.left(170)
+
+    if abs(t.position()) < 1:
+        break
+
+t.end_fill()
+t.exitonclick()
